9d315b15e9fe252e98500bbb38b7508ceaa832ca
policebunny <136323006+policebunny@users.noreply.github.com>
2024-06-17 10:34:33 +0200

    test anim

--- TD-Test-TERRIBLR SFX/Assets/Scripts/BuilderAnimScript.cs
+++ TD-Test-TERRIBLR SFX/Assets/Scripts/BuilderAnimScript.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class BuilderAnimScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Builder selfBob;
-    private Animator _animation;
+    private Builder selfBob;
+    public Animator _animation;
     void Start()
     {
         selfBob = GetComponent<Builder>();
@@ -18,25 +18,43 @@ public class BuilderAnimScript : MonoBehaviour
     {
         if(selfBob.IsBuilding)
         {
+            setAllFalse();
+            _animation.SetBool("isBuilding", true);
             // anim
             // _animation.SetBool("otheranim_name", false);
             // _animation.SetBool("anim_name", true);
         }
         if (selfBob.IsMoving)
         {
+            setAllFalse();
+            _animation.SetBool("isFlying", true);
             // anim
             // _animation.SetBool("otheranim_name", false);
             // _animation.SetBool("anim_name", true);
         }
         if (selfBob.IsIdle)
         {
+            setAllFalse();
+            _animation.SetBool("isIdle", true);
             // anim
             // _animation.SetBool("otheranim_name", false);
             // _animation.SetBool("anim_name", true);
         }
         if (selfBob.DoneBuilding)
         {
-
+            setAllFalse();
+            _animation.SetBool("isIdle", true);
         }
     }
+
+
+    public void setAllFalse()
+    {
+        _animation.SetBool("isBuilding", false);
+        _animation.SetBool("isMoving", false);
+        _animation.SetBool("isFlying", false);
+        _animation.SetBool("isIdle", false);
+    }
+
+
 }
